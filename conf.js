e0cdcce2ac62af646ddb4974312fdd86ad938b10
lokeshkp <kplokesh@gmail.com>
2018-08-12 23:17:04 +0530

    Jasmine repots added in config.js

--- conf.js
+++ conf.js
@@ -1,3 +1,5 @@
+var HtmlReporter = require('protractor-beautiful-reporter');
+
 exports.config={
     framework:'jasmine',
     seleniumAddress: 'http://localhost:4444/wd/hub/',
@@ -11,5 +13,11 @@ exports.config={
         browserName:'chrome'
     }]*/
 
+    onPrepare: function() {
+        // Add a screenshot reporter and store screenshots to `/tmp/screenshots`:
+        jasmine.getEnv().addReporter(new HtmlReporter({
+           baseDirectory: 'JasmineReports/screenshots'
+        }).getJasmine2Reporter());
+     }
 
 };
